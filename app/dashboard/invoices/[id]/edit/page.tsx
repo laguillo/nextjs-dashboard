fed7ed4b439c05328d109116fdda2916c902b759
Eduardo Lago <laguillo@gmail.com>
2025-12-03 23:16:45 -0500

    Renombrar componente de formulario de edición de facturas a EditInvoiceForm

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,8 +1,8 @@
-import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import { notFound } from "next/navigation";
 import { Metadata } from "next";
+import EditInvoiceForm from "@/app/ui/invoices/edit-form";
 
 export const metadata: Metadata = {
   title: "Edit Invoice",
@@ -32,7 +32,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
           },
         ]}
       />
-      <Form invoice={invoice} customers={customers} />
+      <EditInvoiceForm invoice={invoice} customers={customers} />
     </main>
   );
 }
