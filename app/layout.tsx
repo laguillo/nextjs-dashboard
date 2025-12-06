76e21474205cc25c51dae41ae18d60edce3e1b45
Eduardo Lago <laguillo@gmail.com>
2025-12-06 07:46:18 -0500

    feat: add Vercel Analytics integration to layout and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/app/ui/global.css";
 import { inter } from "./ui/fonts";
 import { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: {
@@ -18,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <main>{children}</main>
+        <Analytics />
+      </body>
     </html>
   );
 }
