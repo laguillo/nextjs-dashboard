1aadb28883dab567a5428fb4df9546444f2ed8c1
Eduardo Lago <laguillo@gmail.com>
2025-12-06 07:26:56 -0500

    refactor: update PostCSS configuration and remove Tailwind config file
    - Changed PostCSS configuration to use '@tailwindcss/postcss' plugin.
    - Deleted the Tailwind CSS configuration file as it is no longer needed.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,13 +12,13 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
-    invalid_type_error: "Please select a customer.",
+    message: "Please select a customer.",
   }),
   amount: z.coerce
     .number()
     .gt(0, { message: "Please enter an amount greater than $0." }),
   status: z.enum(["pending", "paid"], {
-    invalid_type_error: "Please select an invoice status.",
+    message: "Please select an invoice status.",
   }),
   date: z.string(),
 });
